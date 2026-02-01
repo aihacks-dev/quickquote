5baa83b8b41aa3cadb7db7af0a3312ff5dfac36d
aihacks-dev <aihacks411@gmail.com>
2026-01-31 20:44:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "quick-lot-scanner-v15";
+const CACHE = "quick-lot-scanner-v17";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=15",
+  "./styles.css?v=17",
   "./app.js",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
@@ -29,5 +29,6 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
 
 
